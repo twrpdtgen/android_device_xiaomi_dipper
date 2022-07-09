d5cce8916b568771ff0b40b5ba0e1f0e3c5b48b7
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-07-09 21:13:54 +0200

    dipper: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: MI 8 Script version: 2.3.0

---
+++ omni_dipper.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from dipper device
+$(call inherit-product, device/xiaomi/dipper/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := dipper
+PRODUCT_NAME := omni_dipper
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 8
+PRODUCT_MANUFACTURER := xiaomi
